a69bcd09da9bdf1d4870adc749b55a007d6e37bf
videhi <anthapuvidehi@gmail.com>
2026-03-11 15:01:19 +0530

    Added app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello, CI/CD world!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from CI/CD demo!"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
